51964d6fa679f206194428f0aeb874167564750f
Paul Mitchum <paul@mile23.com>
2022-06-02 10:57:56 -0700

    user can say ddev dktl

--- dkan-tools/bin/app.php
+++ dkan-tools/bin/app.php
@@ -4,8 +4,17 @@
 use Consolidation\AnnotatedCommand\CommandFileDiscovery;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
-throw new \Exception(realpath(__DIR__));
-require_once __DIR__ . '/../vendor/autoload.php';
+foreach ([
+  // Legacy-style autoload.
+  '/../vendor/autoload.php',
+  // Under vendor.
+  '/../../vendor/autoload.php',
+         ] as $path) {
+  $pathy = __DIR__ . $path;
+  if (file_exists($pathy)) {
+    require_once $pathy;
+  }
+}
 
 putenv("COMPOSER_MEMORY_LIMIT=-1");
 
