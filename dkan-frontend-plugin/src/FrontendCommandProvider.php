f40694faf0717f2975980258d9dca8f6a9f2393d
Paul Mitchum <paul@mile23.com>
2022-06-27 16:00:32 -0700

    adding frontend as a repo

---
+++ dkan-frontend-plugin/src/FrontendCommandProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dkan\Composer\Plugin\Frontend;
+
+use Composer\Plugin\Capability\CommandProvider;
+
+/**
+ * List of all commands provided by this package.
+ *
+ * @internal
+ */
+class FrontendCommandProvider implements CommandProvider {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCommands() {
+    return [new FrontendCommand()];
+  }
+
+}
